0a11e8d45dfb1399172b8c6bd60d16e3b16257cb
pavithranatarajan2000 <58357646+pavithranatarajan2000@users.noreply.github.com>
2020-08-07 20:30:33 -0700

    Update voice assistant.py

--- voice assistant.py
+++ voice assistant.py
@@ -1,107 +1,182 @@
-import speech_recognition as sr
+from __future__ import unicode_literals  
 import pyttsx3
-import datetime
-import wikipedia
 import webbrowser
-import os
-import time
-import subprocess
-from ecapture import ecapture as ec
-import wolframalpha
-import json
-import requests
+import smtplib
 import random
+import speech_recognition as sr
+import wikipedia
+import datetime
+import wolframalpha
+import os
+import sys
+import youtube_dl
+import urllib
+import shutil
+from pathlib import Path
+import requests
+import time
 
 
-print('Loading your AI personal assistant - chitty')
+engine = pyttsx3.init()
+'''len(voices)-1'''
 
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
 engine.setProperty('voice','voices[0].id')
 
-
-def speak(text):
-    engine.say(text)
+def speak(audio):
+    print('Chitty ' + audio)
+    engine.say(audio)
     engine.runAndWait()
 
-def wishMe():
-    hour=datetime.datetime.now().hour
-    if hour>=0 and hour<12:
-        speak("Hello,Good Morning")
-        print("Hello,Good Morning")
-    elif hour>=12 and hour<18:
-        speak("Hello,Good Afternoon")
-        print("Hello,Good Afternoon")
-    else:
-        speak("Hello,Good Evening")
-        print("Hello,Good Evening")
+def greetMe():
+    currentH = int(datetime.datetime.now().hour)
+    if currentH >= 10 and currentH < 3:
+        speak('Hi Pavi,Good Morning! The time is',currentH)
 
-def takeCommand():
-    r=sr.Recognizer()
-    with sr.Microphone() as source:
+    if currentH >= 3 and currentH < 15:
+        speak('Hi Pavi,Good Afternoon!')
+
+    if currentH >= 15 and currentH !=19:
+        speak('Hi Pavi,Good Evening!')
+
+    if currentH >= 19 and currentH !=10:
+        speak('Good Night Pavi! It is night...Sweet Dreams!!')
+        speak ('May i help you?')
+
+greetMe()
+
+speak('I am your assistant Chitty ,How may I help you?')
+
+
+def myCommand():
+   
+    r = sr.Recognizer()                                                                                   
+    with sr.Microphone() as source:                                                                       
         print("Listening...")
-        audio=r.listen(source)
+        r.pause_threshold =  1
+        r.adjust_for_ambient_noise(source, duration=1)
+        audio = r.listen(source)
+        r.pause_threshold =  0.5
+    try:
+        query = r.recognize_google(audio).lower()
+        print('User: ' + query + '\n')
+        
+    except sr.UnknownValueError:
+        speak('Sorry Pavi! TRY Writing your command Pavi')
+        query = str(input('Command: '))
 
-        try:
-            statement=r.recognize_google(audio,language='en-in')
-            print(f"user said:{statement}\n")
-
-        except Exception as e:
-            speak("Pardon me, please say that again")
-            return "None"
-        return statement
-
-speak("Loading your AI personal assistant chitty")
-wishMe()
-
-
-if __name__=='__main__':
-
-
-    while True:
-        speak("Tell me how can I help you now?")
-        statement = takeCommand().lower()
-        if statement==0:
-            continue
-
-        if "good bye" in statement or "ok bye" in statement or "stop" in statement:
-            speak('your personal assistant chitty is shutting down,Good bye')
-            print('your personal assistant chitty is shutting down,Good bye')
-            break
-
-
-
-        if 'wikipedia' in statement:
-            speak('Searching Wikipedia...')
-            statement =statement.replace("wikipedia", "")
-            results = wikipedia.summary(statement, sentences=3)
-            speak("According to Wikipedia")
-            print(results)
-            speak(results)
-
-        elif 'open youtube' in statement:
-            webbrowser.open_new_tab("https://www.youtube.com")
-            speak("youtube is open now")
-            time.sleep(5)
-
-        elif 'open google' in statement:
-            webbrowser.open_new_tab("https://www.google.com")
-            speak("Google chrome is open now")
-            time.sleep(5)
-        elif 'what is your name' in statement:
-            speak("I am Chitty")
-            print("I am Chitty")
-        elif 'what is my name' in statement:
-            speak("Your name is pavithra")
-            time.sleep(5)
+    return query
         
 
-        elif 'open gmail' in statement:
-            webbrowser.open_new_tab("gmail.com")
-            speak("Google Mail open now")
-            time.sleep(5)
-        elif "motivation Quotes" in statement or 'motivate me' in statement:
-            quotes = ['Failure will never overtake me if my determination to succeed is strong enough',
+if __name__ == '__main__':
+
+    while True:
+    
+        query = myCommand();
+        query = query.lower()
+        
+        if 'open youtube' in query:
+            speak('okay')
+            webbrowser.open('www.youtube.com')
+
+        elif 'open google' in query:
+            speak('okay')
+            webbrowser.open('www.google.co.in')
+
+        elif 'open gmail' in query:
+            speak('okay')
+            webbrowser.open('www.gmail.com')
+
+        elif "what\'s up" in query or 'how are you' in query:
+            stMsgs = ['Just doing my thing!', 'I am fine!', 'Nice!', 'I am nice and full of energy']
+            speak(random.choice(stMsgs))
+
+            
+        elif 'email' in query:
+            speak('Who is the recipient? ')
+            recipient = myCommand()
+
+            if 'me' in recipient:
+                try:
+                    speak('What should I say? ')
+                    content = myCommand()
+        
+                    server = smtplib.SMTP('smtp.gmail.com', 587)
+                    server.ehlo()
+                    server.starttls()
+                    server.login("Your_Username", 'Your_Password')
+                    server.sendmail('Your_Username', "Recipient_Username", content)
+                    server.close()
+                    speak('Email sent!')
+
+                except:
+                    speak('Sorry Pavi! I am unable to send your message at this moment!')
+
+
+        elif 'nothing' in query or 'abort' in query or 'stop' in query or 'bye' in query:
+            speak('okay')
+            speak('Bye Pavi, have a good day.')
+            sys.exit()
+
+        elif 'what time is it' in query or 'what is the time Chitty' in query or 'what time is it' in query or 'time' in query or 'current time' in query:
+            speak('The current time is '+(str(datetime.datetime.now().strftime('%H:%M:%S'))))
+
+        elif 'what date is it' in query or 'what is the date Chitty' in query or 'what date is it' in query or 'date' in query or 'current date' in query:
+            speak('The current time is '+(str(datetime.datetime.now().strftime('%H:%M:%S'))))
+
+        elif 'hello' in query:
+            speak('Hello Pavi')
+
+                                    
+        elif 'play music' in query or 'play songs' in query or 'play songs Chitty' in query:
+            music_folder = 'S:\\AI\\music\\'
+            music = ['Alex_Sparrow-sheiscrazybutsheismine','wedont','Konjam','stressedout']
+            random_music = music_folder + random.choice(music) + '.mp3'
+            speak('Okay, here is your music! Enjoy!')
+            
+            os.system(random_music)
+
+        elif 'play english music' in query or 'play english songs' in query or 'play english songs Chitty' in query:
+            music_folder = 'S:\\AI\\music\\'
+            music = ['Alex_Sparrow-sheiscrazybutsheismine','wedont','Konjam','stressedout']
+            random_music = music_folder + random.choice(music) + '.mp3'
+            speak('Okay, here is your music! Enjoy!')
+            
+            os.system(random_music)
+
+        elif 'play tamil music' in query or 'play tamil songs' in query or 'play tamil songs skava' in query:
+            music_folder = 'S:\\AI\\music\\'
+            music = ['Konjam']
+            random_music = music_folder + random.choice(music) + '.mp3'
+            speak('Okay, here is your music! Enjoy!')
+            
+            os.system(random_music)
+
+        elif 'play hindi music' in query or 'play hindi songs' in query or 'play hindi songs skava' in query:
+            music_folder = 'S:\\AI\\music\\'
+            music = ['ikvaariaa']
+            random_music = music_folder + random.choice(music) + '.mp3'
+            speak('Okay, here is your music! Enjoy!')
+            
+            os.system(random_music)
+        elif 'download youtube' in query:
+            ydl_opts = {}
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([input()])
+        elif 'download pdf' in query:
+            filename = Path('DAA.pdf')
+            url = input("Enter the link")
+            res= requests.get(url)
+            filename.write_bytes(res.content)
+        
+
+        elif "log off" in query or "sign out" in query:
+            speak("Ok , your pc will log off in 10 sec make sure you exit from all applications")
+            subprocess.call(["shutdown", "/l"])
+        
+        elif "Tell me motivation Quotes" in query or 'motivate me' or 'say motivational quotes' or 'sad' or 'unhappy' in query:
+            stMsgs = ['Failure will never overtake me if my determination to succeed is strong enough',
                       'The past cannot be changed. The future is yet in your power',
                       'Only I can change my life. No one can do it for me',
                       'Change your life today. Don\'t gamble on the future, act now, without delay',
@@ -110,90 +185,33 @@ if __name__=='__main__':
                       'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time',
                       'Good, better, best. Never let it rest. Till your good is better and your better is best']
             highMsgs = ['Dont worry dude,every hard time comes to an end']
-            speak(random.choice(quotes))
-            speak('I think thins Motivated You sir ... if Not')
+            speak(random.choice(stMsgs))
+            speak('I think thins Motivated You Pavi ... if Not')
             speak(random.choice(highMsgs))
-            time.sleep(5)
+            speak('You will be happy in few minutes watch this')
+            webbrowser.open('https://www.youtube.com/watch?v=20p6inVwjbI')
+            time.sleep(60)
+            speak('I think you cool now!!')
 
-        elif "weather" in statement:
-            api_key="8ef61edcf1c576d65d836254e11ea420"
-            base_url="https://api.openweathermap.org/data/2.5/weather?"
-            speak("whats the city name")
-            city_name=takeCommand()
-            complete_url=base_url+"appid="+api_key+"&q="+city_name
-            response = requests.get(complete_url)
-            x=response.json()
-            if x["cod"]!="404":
-                y=x["main"]
-                current_temperature = y["temp"]
-                current_humidiy = y["humidity"]
-                z = x["weather"]
-                weather_description = z[0]["description"]
-                speak(" Temperature in kelvin unit is " +
-                      str(current_temperature) +
-                      "\n humidity in percentage is " +
-                      str(current_humidiy) +
-                      "\n description  " +
-                      str(weather_description))
-                print(" Temperature in kelvin unit = " +
-                      str(current_temperature) +
-                      "\n humidity (in percentage) = " +
-                      str(current_humidiy) +
-                      "\n description = " +
-                      str(weather_description))
-
-            else:
-                speak(" City Not Found ")
-
-
-
-        elif 'time' in statement:
-            strTime=datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"the time is {strTime}")
-
-        elif 'who are you' in statement or 'what can you do' in statement:
-            speak('I am chitty version 1 point O your persoanl assistant. I am programmed to minor tasks like'
-                  'opening youtube,google chrome,gmail and stackoverflow ,predict time,take a photo,search wikipedia,predict weather' 
-                  'in different cities , get top headline news from times of india and you can ask me computational or geographical questions too!')
-
-
-        elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
-            speak("I was built by Pavithra")
-            print("I was built by Pavithra")
-
-        elif "open stackoverflow" in statement:
-            webbrowser.open_new_tab("https://stackoverflow.com/login")
-            speak("Here is stackoverflow")
-
-        elif 'news' in statement:
-            news = webbrowser.open_new_tab("https://timesofindia.indiatimes.com/home/headlines")
-            speak('Here are some headlines from the Times of India,Happy reading')
-            time.sleep(6)
-
-        elif "camera" in statement or "take a photo" in statement:
-            ec.capture(0,"robo camera","img.jpg")
-
-        elif 'search'  in statement:
-            statement = statement.replace("search", "")
-            webbrowser.open_new_tab(statement)
-            time.sleep(5)
-
-        elif 'ask' in statement:
-            speak('I can answer to computational and geographical questions and what question do you want to ask now')
-            question=takeCommand()
-            app_id="R2K75H-7ELALHR35X"
-            client = wolframalpha.Client('R2K75H-7ELALHR35X')
-            res = client.query(question)
-            answer = next(res.results).text
-            speak(answer)
-            print(answer)
-
-
-        elif "log off" in statement or "sign out" in statement:
-            speak("Ok , your pc will log off in 10 sec make sure you exit from all applications")
-            subprocess.call(["shutdown", "/l"])
-
-time.sleep(3)
-
-
+        else:
+            query = query
+            speak('Searching...')
+            try:
+                try:
+                    res = client.query(query)
+                    results = next(res.results).text
+                    speak('google says - ')
+                    speak('Got it.')
+                    speak(results)
+                    
+                except:
+                    results = wikipedia.summary(query, sentences=2)
+                    speak('Got it.')
+                    speak('WIKIPEDIA says - ')
+                    speak(results)
+        
+            except:
+                webbrowser.open('www.google.com')
+        
+        speak('any Command! Pavi!')
 
